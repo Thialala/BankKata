5dca822d5de1c438bd27838f4f1b5a470c3e2f0c
Ousmane <ousmane.thialala@gmail.com>
2018-09-17 00:12:45 +0200

    Added acceptance tests for money transfer from a payer account to a payee account

---
+++ BankKata.AcceptanceTests/AccountSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BankKata.AcceptanceTests
+{
+    [Binding]
+    public class SuccessfulMoneyTransferSteps
+    {
+        [Given(@"a payer account with initial balance of €(.*)")]
+        public void GivenAPayerAccountWithInitialBalanceOf(decimal payerInitialBalance)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a payee account with initial balance of €(.*)")]
+        public void GivenAPayeeAccountWithInitialBalanceOf(decimal payeeInitialBalance)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the payer transfers €(.*) to the payee")]
+        public void WhenThePayerTransfersToThePayee(decimal amountToTransfer)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the balance of payer account should be €(.*)")]
+        public void ThenTheBalanceOfPayerAccountShouldBe(decimal payerBalance)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the balance of the payee account should be €(.*)")]
+        public void ThenTheBalanceOfThePayeeAccountShouldBe(decimal payeeBalance)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

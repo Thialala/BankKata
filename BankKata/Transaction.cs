76f7c3764c03d4296be5820be9813039b9d8f8a9
Thialala <ousmane.thialala@gmail.com>
2018-09-17 16:52:06 +0200

    Move Account and Transaction to their own library class

--- BankKata/Transaction.cs
+++ BankKata/Transaction.cs
@@ -1,46 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace BankKata.AcceptanceTests
+namespace BankKata
 {
-    public class Account
-    {
-        public int Id { get; }
-
-        public decimal Balance { get; private set; }
-
-        private readonly List<Transaction> _transactions = new List<Transaction>();
-        private readonly Func<DateTime> _dateProvider;
-
-        public IReadOnlyList<Transaction> Transactions => _transactions.AsReadOnly();
-
-        public Account(int id, decimal initialBalance, Func<DateTime> dateProvider = null)
-        {
-            Id = id;
-            Balance = initialBalance;
-            _dateProvider = dateProvider ?? (() => DateTime.Today);
-        }
-
-        public void Transfer(decimal amount, Account toAccount)
-        {
-            Balance -= amount;
-            toAccount.Receive(amount, this);
-            _transactions.Add(new Transaction(-amount, _dateProvider(), Id, toAccount.Id));
-        }
-
-        private void Receive(decimal amount, Account fromAccount)
-        {
-            Balance += amount;
-            _transactions.Add(new Transaction(amount, _dateProvider(), fromAccount.Id, Id));
-        }
-
-        public IEnumerable<Transaction> GetTransactionHistoryFromOrTo(int accountNumber)
-        {
-            return _transactions.Where(t => t.IsFromOrTo(accountNumber));
-        }
-    }
-
     public class Transaction
     {
         private readonly decimal _amount;
